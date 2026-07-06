0cce13defeaa3f2afddeda79681917f7e7bea6da
gazraim-png <gazraim@example.com>
2026-07-06 18:08:22 +0500

    Configure GitHub Pages deployment

--- index.tsx
+++ index.tsx
@@ -1,19 +1,19 @@
 import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error('Root element not found. Please ensure there is a <div id="root"></div> in your index.html.');
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
